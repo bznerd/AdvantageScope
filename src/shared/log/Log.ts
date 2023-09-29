036b038f364f8cbde5f0ac209bd4caf68fb2d59b
Jonah <47046556+jwbonner@users.noreply.github.com>
2023-09-29 00:25:01 -0400

    Enable struct & protobuf

--- src/shared/log/Log.ts
+++ src/shared/log/Log.ts
@@ -256,9 +256,9 @@ export default class Log {
     }
 
     // Check for struct schema
-    // if (key.includes("/.schema/struct:")) {
-    //   this.structDecoder.addSchema(key.split("struct:")[1], value);
-    // }
+    if (key.includes("/.schema/struct:")) {
+      this.structDecoder.addSchema(key.split("struct:")[1], value);
+    }
   }
 
   /** Writes a new Boolean value to the field. */
